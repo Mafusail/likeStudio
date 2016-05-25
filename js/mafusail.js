47d9a5b4ddb330a3b9e743c396f28e6cbfceea69
Mafusail <shapovalenko.a.a@gmail.com>
2016-05-25 04:12:10 +0300

    Done form, valideit, php(mail)

--- js/mafusail.js
+++ js/mafusail.js
@@ -58,4 +58,69 @@ $(document).ready(function(){
 
 
 
+
+
+
+
+    // Заказ
+
+    $('#button_a').click(function () {
+        $("#block_7_a").show(500);
+        $("#hover_a").show();
+    });
+    $('#hover_a').click(function () {
+        $("#block_7_a").hide();
+        $("#hover_a").hide();
+    });
+
+
+    /*validate form*/
+
+    $("#loginform").validate({
+
+        rules: {
+
+            imya: {
+                required: true,
+            },
+
+            email: {
+                email: true,
+                required: true,
+            },
+
+            phone: {
+                required: true,
+                digits: true,
+                minlength: 10,
+                maxlength: 10,
+            },
+        },
+
+        messages: {
+
+            imya: {
+                required: "Поле 'Имя' <br>обязательно для заполнения!",
+            },
+
+            email: {
+                required: "Поле для e-mail обязательно <br> для заполнения!",
+                email: "В e-mail адресе должен <br> присудстовать символ '@'! <br> Примере: LIKESTIDIO@GMAIL.COM",
+            },
+
+            phone: {
+                required: "Поле 'Номер телефон' обязательно <br> для заполнения!",
+                digits: "Это поле содержит только цифры <br> Пример:  0631234567",
+                minlength: "Пароль должен быть не менее <br> 10 символов",
+                maxlength: "Пароль должен быть не менее <br> 10 символов",
+            },
+
+        }
+
+    });
+
+
+
+
+
 });
---
+++ php/mail.php
@@ -0,0 +1,38 @@
+<?php
+
+$to = "shapovalenko.a.a@gmail.com";
+$tema = "Новый заказ в компанию Like Studio";
+$message = "Имя пользователя: " .$_POST['imya']. "<br>";
+$message .= "Номер телефон: " .$_POST['phone']. "<br>";
+$message .= "Email: " .$_POST['email']. "<br>";
+$message .= "Комментарии: " .$_POST['message']. "<br>";
+$message .= "Дополнительные файлы: " .$_POST['maket'];
+
+$headers = "From: $from_user <$from_email>\r\n".
+    "MIME-Version: 1.0" . "\r\n" .
+    "Content-type: text/html; charset=UTF-8" . "\r\n";
+
+mail($to, $tema, $message, $headers);
+
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Запрос</title>
+    </head>
+
+    <body>
+        <h1>Ваш заказ отправлен на обработку.</h1>
+        <h2>В ближайшие время наш менеджер свяжется с Вами</h2>
+        <h3>Спасибо Вам, за обращение в Like Studio</h3>
+        <script type="text/javascript">
+             setTimeout('location.replace("/sh1/likeStudio/index.html")', 5000);
+        </script>
+
+    </body>
+
+    </html>
