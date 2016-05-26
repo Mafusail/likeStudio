a86bcef4208498e933314bfe884f4369a8db03f8
MaxSuslov <maxsusmail@gmail.com>
2016-05-26 10:54:50 +0300

    Skills Expertise + Mediaqueries

---
+++ js/max.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+    $('.htmlcss').mouseenter(function(){
+        $(this).attr('src', 'img/1htmlcss_300x300.jpg');
+    });
+
+    $('.htmlcss').mouseleave(function(){
+        $(this).attr('src', 'img/1html_300x300.jpg');
+    });
+
+
+
+    $('.jquery').mouseenter(function(){
+        $(this).attr('src', 'img/2jqueryajaxx_300x300.jpg');
+    });
+
+    $('.jquery').mouseleave(function(){
+        $(this).attr('src', 'img/2jqueryajax_300x300.jpg');
+    });
+
+
+
+    $('.git').mouseenter(function(){
+        $(this).attr('src', 'img/3github_300x300.jpg');
+    });
+
+    $('.git').mouseleave(function(){
+        $(this).attr('src', 'img/3git_300x300.jpg');
+    });
+
+
+
+    $('.ruby').mouseenter(function(){
+        $(this).attr('src', 'img/5haml-sass_300x300.jpg');
+    });
+
+    $('.ruby').mouseleave(function(){
+        $(this).attr('src', 'img/4comp-rub_300x300.jpg');
+    });
+
+
+
+    $('.hamlsass').mouseenter(function(){
+        $(this).attr('src', 'img/1htmlcss_300x300.jpg');
+    });
+
+    $('.hamlsass').mouseleave(function(){
+        $(this).attr('src', 'img/5haml-sass_300x300.jpg');
+    });
+
+
+
+    $('.responsive').mouseenter(function(){
+        $(this).attr('src', 'img/6responsive-design_300x300.jpg');
+    });
+
+    $('.responsive').mouseleave(function(){
+        $(this).attr('src', 'img/6responsive_300x300.jpg');
+    });
+
+
+
+    $('.frameworks').mouseenter(function(){
+        $(this).attr('src', 'img/1htmlcss_300x300.jpg');
+    });
+
+    $('.frameworks').mouseleave(function(){
+        $(this).attr('src', 'img/7bootstrap-mater_300x300.jpg');
+    });
+
+
+
+    $('.wordpress').mouseenter(function(){
+        $(this).attr('src', 'img/1htmlcss_300x300.jpg');
+    });
+
+    $('.wordpress').mouseleave(function(){
+        $(this).attr('src', 'img/8wordpress_300x300.jpg');
+    });
+
+
+
+
+});
+
